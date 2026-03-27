c4b5a86591fba6289aa71c2f6f51ced0319a73fb
图图 <70736550+RemindofAgustD@users.noreply.github.com>
2026-03-27 07:08:45 +0000

    修改 pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,10 +2,13 @@ import sys
 import pandas as pd
 
 print("arguments", sys.argv)
-day = int(sys.argv[1])
-print(f"Running pipeline for day {day}")
 
+month = int(sys.argv[1])
 
-df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df = pd.DataFrame({'day':[1,2],'num_passengers':[3,4]})
+df['month'] = month
 print(df.head())
-df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+df.to_parquet(f'output_{month}.parquet')
+
+print(f'hello pipeline,month={month}')
